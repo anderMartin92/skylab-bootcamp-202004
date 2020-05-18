fae20daac1ab9da22e394205423f813bfeaf8748
Ander Martin Ripa <andermartinr@gmail.com>
2020-05-18 18:33:15 +0200

    list contact

--- staff/ander/node/contacts/index.js
+++ staff/ander/node/contacts/index.js
@@ -1,48 +1,10 @@
-const readline = require('readline')
-const fs = require('fs')
+const addContact = require('./logic/add-contact')
+const listContacts = require('./logic/list-contacts')
 
-const prompt =readline.createInterface({
+addContact('name', 'surname', 'phone', 'email', 'age', 'birthdate', 'country')
+// listContacts()
 
-    input: process.stdin,
-    output: process.stdout
-})
-
-const contact = {
-    
-}
-
-prompt.question("What's your name?", name =>{
-    contact.name = name
-
-    prompt.question("What's your surname?", surname =>{
-        contact.surname = surname
-
-        prompt.question("What's your phone ?", phone =>{
-            contact.phone = phone
-
-            prompt.question("What's your email ?", email =>{
-                contact.email = email
-
-                prompt.question("What's your URL?", url =>{
-                    contact.url = url
-
-                    prompt.question("What's your instagram?", instagram=>{
-                        contact.instagram = instagram
-
-                        prompt.question("What's your facebook?", facebook =>{
-                            contact.facebook = facebook
-
-                            prompt.question("What's your twitter?", twitter =>{
-                                contact.twitter = twitter
-
-                                prompt.question("What's your fwitter?", fwitter =>{
-                                    contact.fwitter = fwitter
-
-                                    prompt.question("What's your tiktok?", tiktok =>{
-                                        contact.tiktok = tiktok
-                                        
-                                        const jsonContact = JSON.stringify(contact)
-                                        let cont=0
+/* 
                                         function checkName(title) {
                                             let itExists = false;
                                             cont++
@@ -68,13 +30,4 @@ prompt.question("What's your name?", name =>{
                                            if (error) throw error
                                            prompt.close()
                                         })
-                                    })
-                                })
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    })
-})
+                              */
